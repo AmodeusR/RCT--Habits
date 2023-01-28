3fc176c8f3eedc357b2b9da1aefee557648b7ec5
Ricardo Magalhães <ricardogleiss@hotmail.com>
2023-01-27 21:14:54 -0300

    Finished server

---
+++ server/prisma/migrations/20230120015525_created_at_as_not_optional/migration.sql
@@ -0,0 +1,18 @@
+/*
+  Warnings:
+
+  - Made the column `created_at` on table `habits` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_habits" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL
+);
+INSERT INTO "new_habits" ("created_at", "id", "title") SELECT "created_at", "id", "title" FROM "habits";
+DROP TABLE "habits";
+ALTER TABLE "new_habits" RENAME TO "habits";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
